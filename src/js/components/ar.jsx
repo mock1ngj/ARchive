0b2b5c2a97a80d5ee4facdbab60d547165c26f50
mock1ngj <randomizedgg9@gmail.com>
2024-02-01 20:48:13 +0800

    ar-ui no event

--- src/js/components/ar.jsx
+++ src/js/components/ar.jsx
@@ -5,8 +5,9 @@ import { FaQuestionCircle } from "react-icons/fa";
 import { FaPlayCircle } from "react-icons/fa";
 import { IconContext } from 'react-icons';
 
-export default ({page}) => {
+export default ({ page }) => {
     const sceneRef = useRef(null);
+    const clickable = useRef(null);
 
     useEffect(() => {
         const sceneEl = sceneRef.current;
@@ -18,27 +19,31 @@ export default ({page}) => {
 
     return (
         <>
-            <IconContext.Provider value={{ color:"#4e9f3d" , className: "faq-button"}}>
-                <div style={{position: 'absolute', top: '2vh', right: '5vw', zIndex:1000}}>
-                    <FaQuestionCircle onClick={()=>{page('home')}}></FaQuestionCircle>
+            <IconContext.Provider value={{ color: "#4e9f3d", className: "faq-button" }}>
+                <div style={{ position: 'absolute', top: '2vh', right: '5vw', zIndex: 1000 }}>
+                    <FaQuestionCircle onClick={() => { page('home') }}></FaQuestionCircle>
                 </div>
             </IconContext.Provider>
             <IconContext.Provider value={{ color: "#4e9f3d", className: "play-button" }}>
-                <div style={{position:'absolute', bottom:'20vh', width: '100%', zIndex:1000}}>
-                    <FaPlayCircle onClick={()=>{page('home')}}></FaPlayCircle>
+                <div style={{ position: 'absolute', bottom: '20vh', width: '100%', zIndex: 1000 }}>
+                    <FaPlayCircle onClick={() => { page('home') }}></FaPlayCircle>
                 </div>
             </IconContext.Provider>
-            <a-scene ref={sceneRef} mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;" color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+            <a-scene ref={sceneRef} mindar-image="filterMinCF:1; filterBeta:500; imageTargetSrc: /targets.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;" color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-assets>
-                    <img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.png" />
-                    <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
+                    <img id="card" src="/ar-ui/card.png" />
+                    <img id="next" src="/ar-ui/next.png" />
+                    <img id="prev" src="/ar-ui/prev.png" />
                 </a-assets>
 
-                <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+                <a-camera position="0 0 0" look-controls="enabled: false" cursor="fuse: false; rayOrigin: mouse;" raycaster="far: ${customFields.libVersion}; objects: .clickable"></a-camera>
 
                 <a-entity mindar-image-target="targetIndex: 0">
-                    <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-                    <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
+                    <a-image ref={clickable} class="clickable" src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0" onClick={() => { page('home'); }}></a-image>
+                    <a-entity position="0 0 0">
+                        <a-image src="#next" position="0.6 0" height="0.125" width="0.125" rotation="0 0 0"></a-image>
+                        <a-image src="#prev" position="-0.6 0 0" height="0.125" width="0.125" rotation="0 0 0"></a-image>
+                    </a-entity>
                 </a-entity>
             </a-scene>
         </>
