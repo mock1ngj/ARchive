8a0d32c018595d76e680b87da30736a9c8495383
mock1ngj <randomizedgg9@gmail.com>
2024-02-04 22:21:03 +0800

    rework on looping mechanics

--- src/js/components/ar.jsx
+++ src/js/components/ar.jsx
@@ -4,23 +4,41 @@ import 'mind-ar/dist/mindar-image-aframe.prod.js';
 import { FaQuestionCircle } from "react-icons/fa";
 import { FaPlayCircle } from "react-icons/fa";
 import { IconContext } from 'react-icons';
+import { showArtifact } from '../main';
 
 export default ({ page }) => {
     const sceneRef = useRef(null);
     const clickable = useRef(null);
-    const [artifact, setArtifact] = useState([true, false, false]);
+    const section = useRef(null);
+    const showArt = new showArtifact(); 
+    const [artifact, setArtifact] = useState({});
     const [ctr, setCtr] = useState(0);
+    const synth = window.speechSynthesis;
+    const [artifactSection, setArtifactSection] = useState(null);
+
+    const speech = () => {
+        const utterance = new SpeechSynthesisUtterance('Testing API');
+        synth.speak(utterance);
+    }
 
     //Next Loop Mechanic
     const next = () => {
-        setCtr((ctr) => ctr != 2 ? ctr + 1 : ctr = 0);
-        const nextArtifact = artifact.map((show, i) => {
-            if (i == ctr) {
-                return show = true;
+        const section = scene.current;
+        section.addEventListener('targetFound', event => {
+            //get current section
+            temp = event.target.getAttribute('data-section');
+            if (temp != artifactSection) {
+                setCtr(0);
+                showArt.reset(artifact.section);
             }
-            return show = false
-        })
-        setArtifact(nextArtifact);
+            setArtifactSection(temp);
+        });
+
+        //TODO: retrieve the number of artifacts per section
+        setCtr((ctr) => ctr != 2 ? ctr + 1 : ctr = 0);
+        const nextArtifact = showArt(artifact);
+
+        setArtifact({ artifactSection: nextArtifact });
     }
 
     //Previous Loop Mechanic
@@ -54,7 +72,7 @@ export default ({ page }) => {
             </IconContext.Provider>
             <IconContext.Provider value={{ color: "#4e9f3d", className: "play-button" }}>
                 <div style={{ position: 'absolute', bottom: '20vh', width: '100%', zIndex: 1000 }}>
-                    <FaPlayCircle onClick={() => { page('home') }}></FaPlayCircle>
+                    <FaPlayCircle onClick={() => speech()}></FaPlayCircle>
                 </div>
             </IconContext.Provider>
             <a-scene ref={sceneRef} mindar-image="filterMinCF:5; filterBeta:2000; imageTargetSrc: /targets.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;" color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
@@ -68,8 +86,7 @@ export default ({ page }) => {
 
                 <a-camera position="0 0 0" look-controls="enabled: false" cursor="fuse: false; rayOrigin: mouse;" raycaster="far: ${customFields.libVersion}; objects: .clickable"></a-camera>
 
-                <a-entity mindar-image-target="targetIndex: 0">
-
+                <a-entity ref={section} data-section="test" mindar-image-target="targetIndex: 0">
                     <a-entity position="0 0 0">
                         <a-plane color="#4E9F3D" height="0.552" width="1"></a-plane>
                         <a-entity visible={artifact[0]}>
@@ -81,21 +98,18 @@ export default ({ page }) => {
                             <a-entity geometry="primitive:plane; height: 0; width: 0;"
                                 text="value:Hunger Games; align:center; width: 0.4; color: black;"
                                 position="0.24 0 0.1"
-                                material="color: white"
-                            >
+                                material="color: white">
                             </a-entity>
                         </a-entity>
                         <a-entity visible={artifact[1]}>
-                            <a-image visible={artifact[1]}
-                                src="#petey"
+                            <a-image src="#petey"
                                 height="0.4"
                                 width="0.4"
                                 position="-0.24 0 0.1">
                             </a-image>
                         </a-entity>
                         <a-entity visible={artifact[2]}>
-                            <a-image visible={artifact[2]}
-                                src="#logo"
+                            <a-image src="#logo"
                                 height="0.4"
                                 width="0.4"
                                 position="-0.24 0 0.1">
@@ -104,6 +118,10 @@ export default ({ page }) => {
                         <a-image ref={clickable} class="clickable" onClick={() => next()} src="#next" position="0.6 0 0" height="0.125" width="0.125" rotation="0 0 0"></a-image>
                         <a-image ref={clickable} class="clickable" onClick={() => prev()} src="#prev" position="-0.6 0 0" height="0.125" width="0.125" rotation="0 0 0"></a-image>
                     </a-entity>
+                </a-entity>
+
+                <a-entity ref={section} data-section="test2" mindar-image-target="targetIndex: 1">
+
                 </a-entity>
             </a-scene>
         </>
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
-import { signal } from "@preact/signals";
+//show and hide with reset
+export class showArtifact {
 
-export const state = () =>{
-    const page = signal('home')
-    return {page};
+    show(array, artifact) {
+        for (let index = 0; index < array.length; index++) {
+            array[index] = false;
+
+            if (index == artifact) {
+                array[index] = true;
+            }
+        }
+        return array;
+    }
+
+    reset(array) {
+        for (let index = 0; index < array.length; index++) {
+            array[index] = false; 
+        }
+        array[0] = true;
+        return array;
+    }
 }
+
+const artifact = new showArtifact();
+var art = artifact.show([true, false, false], 2);
+console.log(art);
+art = artifact.reset(art);
+console.log(art);
